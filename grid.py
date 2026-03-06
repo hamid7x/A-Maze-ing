822ad090345888d6f9cba10d11313f0e3c67f241
Abdessamad Oukaamouch <aoukaamo@e3r10p14.1337.ma>
2026-03-06 15:14:18 +0000

    feat: ASCII terminal renderer with thick walls

--- grid.py
+++ grid.py
@@ -1,16 +1,16 @@
 class Grid:
+
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.grid = self.build_grid()
+        self.grid = []
+
     def build_grid(self):
-        grid = []
         for _ in range(self.height):
             row = []
             for _ in range(self.width):
                 row.append(0xf)
-            grid.append(row)
-        return grid
+            self.grid.append(row)
     
     def get_cell(self, x, y):
         if (0 <= x < self.width and 0 <= y < self.height):
@@ -20,11 +20,3 @@ class Grid:
     def set_cell(self, x, y, val) -> None:
         if (0 <= x < self.width and 0 <= y < self.height):
             self.grid[y][x] = val
-
-rg = Grid(4, 4)
-rg.build_grid()
-print(rg.get_cell(2,4))
-rg.set_cell(2,3,0x0)
-print(rg.get_cell(2,3))
-print(rg.grid)
-rg.renderer()
